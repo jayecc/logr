d58f76dccc8066a195f1ccb96daaf7f547de71c3
叶嘉 <jayec@foxmail.com>
2024-03-22 17:59:42 +0800

    up

--- output.go
+++ output.go
@@ -36,3 +36,35 @@ func (l *Logger) Error(msg string, fields ...zap.Field) {
 func (l *Logger) Errorf(format string, args ...interface{}) {
 	l.logger.Error(fmt.Sprintf(format, args...))
 }
+
+func Debug(msg string, fields ...zap.Field) {
+	Default().Debug(msg, fields...)
+}
+
+func Debugf(format string, args ...interface{}) {
+	Default().Debugf(format, args...)
+}
+
+func Info(msg string, fields ...zap.Field) {
+	Default().Info(msg, fields...)
+}
+
+func Infof(format string, args ...interface{}) {
+	Default().Infof(format, args...)
+}
+
+func Warn(msg string, fields ...zap.Field) {
+	Default().Warn(msg, fields...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	Default().Warnf(format, args...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	Default().Error(msg, fields...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	Default().Errorf(format, args...)
+}
